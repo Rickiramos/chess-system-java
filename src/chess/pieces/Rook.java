97fb3b16ba071ac4b733d139bd9d26d8eb7ed959
Ricardo Ramos <mtb.ricardo2014@gmail.com>
2021-08-10 22:46:45 -0300

    Possible moves of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -15,5 +15,11 @@ public String toString() {
 	return "R";
 }
 
+@Override
+public boolean[][] possibleMoves() {
+	boolean [][] mat = new boolean [getBoard().getRows()][getBoard().getColumns()];
+	return mat;
+}
+
 
 }
